af2cc4cf70aea316b8602cf8dece82b3bb9a8c94
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-07 08:24:26 +0200

    reworked eevee

--- src/pokemon/gmax_133_eevee.lua
+++ src/pokemon/gmax_133_eevee.lua
@@ -3,23 +3,16 @@ local gmax_eevee = {
   name = "gmax_eevee",
   pos = { x = 0, y = 8 },
   soul_pos = { x = 1, y = 8 },
-  config = { extra = { Xmult_mod = 1.25 } },
+  config = { extra = { Xmult = 2.66 } },
   loc_txt = {
     name = "Gigantamax Eevee",
     text = {
-      "{C:white,X:mult}X#3#{} Mult for each",
-      "adjacent Joker",
-      "{C:inactive}(Currently {C:white,X:mult}X#4#{C:inactive})",
+      "{C:white,X:mult}X#3#{} Mult",
     }
   },
   loc_vars = function(self, info_queue, center)
     type_tooltip(self, info_queue, center)
-    local current_Xmult = 0
-    -- No way to count adjacent jokers in the collection screen
-    if center.area and center.area.config and not center.area.config.collection then
-      current_Xmult = center.ability.extra.Xmult_mod * #poke_get_adjacent_jokers(center)
-    end
-    return { vars = { center.ability.extra.Xmult_mod, current_Xmult } }
+    return { vars = { center.ability.extra.Xmult } }
   end,
   rarity = "agar_gmax",
   cost = 12,
@@ -30,13 +23,9 @@ local gmax_eevee = {
   blueprint_compat = true,
   calculate = function(self, card, context)
     if context.joker_main then
-      local current_Xmult = card.ability.extra.Xmult_mod * #poke_get_adjacent_jokers(card)
-      if current_Xmult > 1 then
-        return {
-          message = localize("agar_gmax_cuddle_ex"),
-          Xmult = current_Xmult,
-        }
-      end
+      return {
+        Xmult = card.ability.extra.Xmult,
+      }
     end
   end,
 }
