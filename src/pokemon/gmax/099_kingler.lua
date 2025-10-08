774282ce0d640be9c5ec1ee8af3a5eb525d494a7
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-08 11:26:50 +0200

    added tentative kingler effect

--- src/pokemon/gmax/099_kingler.lua
+++ src/pokemon/gmax/099_kingler.lua
@@ -3,16 +3,17 @@ local gmax_kingler = {
   name = "gmax_kingler",
   pos = { x = 12, y = 7 },
   soul_pos = { x = 13, y = 7 },
-  config = { extra = {  } },
+  config = { extra = { chips = 16 } },
   loc_txt = {
     name = "Gigantamax Kingler",
     text = {
-      "{C:inactive}Does nothing (yet!)"
+      "Played face cards permanently",
+      "get {C:chips}+#3#{} chips when scored",
     }
   },
   loc_vars = function(self, info_queue, center)
     type_tooltip(self, info_queue, center)
-    return { vars = {} }
+    return { vars = { center.ability.extra.chips } }
   end,
   rarity = "agar_gmax",
   cost = 10,
@@ -22,6 +23,11 @@ local gmax_kingler = {
   atlas = "AtlasJokersBasicGen01",
   blueprint_compat = true,
   calculate = function(self, card, context)
+    context.other_card.ability.perma_bonus = (context.other_card.ability.perma_bonus or 0) + card.ability.extra.chips
+    return {
+      message = localize('k_upgrade_ex'),
+      colour = G.C.CHIPS
+    }
   end,
 }
 
