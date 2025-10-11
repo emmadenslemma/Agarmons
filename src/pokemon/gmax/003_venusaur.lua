cf904026336f0e2b58d31d556d7b51a10147a4b9
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-11 04:24:27 +0200

    add venusaur

--- src/pokemon/gmax/003_venusaur.lua
+++ src/pokemon/gmax/003_venusaur.lua
@@ -3,16 +3,17 @@ local gmax_venusaur = {
   name = "gmax_venusaur",
   pos = { x = 12, y = 6 },
   soul_pos = { x = 13, y = 6 },
-  config = { extra = {} },
+  config = { extra = { draw_mod = 1 } },
   loc_txt = {
     name = "Gigantamax Venusaur",
     text = {
-      "{C:inactive}Does nothing (yet!)"
+      "Every hand played",
+      "draws {C:attention}+#3#{} cards"
     }
   },
   loc_vars = function(self, info_queue, center)
     type_tooltip(self, info_queue, center)
-    return { vars = {} }
+    return { vars = { center.ability.extra.draw_mod } }
   end,
   rarity = "agar_gmax",
   cost = 12,
@@ -22,6 +23,13 @@ local gmax_venusaur = {
   atlas = "AtlasJokersBasicGen01",
   blueprint_compat = true,
   calculate = function(self, card, context)
+    if context.before and context.cardarea == G.jokers then
+      card_eval_status_text(card, "extra", nil, nil, nil, {
+        message = localize("agar_gmax_vine_lash_ex"),
+        colour = G.C.RARITY["agar_gmax"]
+      })
+      G.FUNCS.draw_from_deck_to_hand(card.ability.extra.draw_mod)
+    end
   end,
 }
 
