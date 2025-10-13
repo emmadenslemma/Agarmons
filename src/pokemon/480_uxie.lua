a7d7eb0221b201976f77e6e3c2568c2daa42ec53
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-13 05:44:12 +0200

    add cbmx's uxie

---
+++ src/pokemon/480_uxie.lua
@@ -0,0 +1,66 @@
+-- Uxie 480
+local uxie = {
+  name = "uxie",
+  config = { extra = { scry = 5, Xmult = 1, Xmult_mod = 1, gold_cards_triggered = 0 }, trigger_rqmt = 11 },
+  loc_txt = {
+    name = "Uxie",
+    text = {
+      "{C:purple}+#1# Foresight",
+      "Gains {C:white,X:mult}X#2#{} Mult every {C:attention}#3# {C:inactive}[#4#]",
+      "{C:attention}Gold{} cards triggered",
+      "{C:inactive}(Currently {C:white,X:mult}X#5#{C:inactive} Mult)",
+      "{br:2}ERROR - CONTACT STEAK",
+      "{C:attention}Foreseen{} cards trigger",
+      "held in hand effects",
+    }
+  },
+  loc_vars = function(self, info_queue, center)
+    type_tooltip(self, info_queue, center)
+    return {
+      vars = {
+        center.ability.extra.scry,
+        center.ability.extra.Xmult_mod,
+        self.config.trigger_rqmt,
+        self.config.trigger_rqmt - center.ability.extra.gold_cards_triggered,
+        center.ability.extra.Xmult,
+      }
+    }
+  end,
+  designer = "CBMX",
+  rarity = 4,
+  cost = 20,
+  stage = "Legendary",
+  ptype = "Psychic",
+  gen = 4,
+  enhancement_gate = "m_gold",
+  calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.hand and not context.end_of_round
+        and SMODS:has_enhancement(context.other_card, "m_gold") then
+      card.ability.extra.gold_cards_triggered = card.ability.extra.gold_cards_triggered + 1
+      if card.ability.extra.gold_cards_triggered == self.config.trigger_rqmt then
+        card.ability.extra.gold_cards_triggered = 0
+        card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_mod
+        return {
+          message = localize("k_upgrade_ex"),
+          colour = G.C.MULT,
+        }
+      end
+    end
+    if context.joker_main then
+      return {
+        Xmult = card.ability.extra.Xmult
+      }
+    end
+  end,
+  add_to_deck = function(self, card, from_debuff)
+    G.GAME.scry_amount = (G.GAME.scry_amount or 0) + card.ability.extra.scry
+  end,
+  remove_from_deck = function(self, card, from_debuff)
+    G.GAME.scry_amount = math.max(0, (G.GAME.scry_amount or 0) - card.ability.extra.scry)
+  end,
+}
+
+return {
+  enabled = agarmons_config.lake_trio or false,
+  list = { uxie }
+}
