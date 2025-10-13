52dda469721948d413182b8fdcb5e5d68c073b2c
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-14 00:15:21 +0200

    fixed crash during end of round

--- src/functions/effects.lua
+++ src/functions/effects.lua
@@ -4,7 +4,7 @@ function effects.apply_scry_hand_effects()
   return SMODS.find_card('j_agar_uxie', false)[1]
       or SMODS.find_card('j_agar_mesprit', false)[1]
       or SMODS.find_card('j_agar_azelf', false)[1]
-      or SMODS.find_card('j_agar_lunala', false)[1].ability.extra.full_active
+      or (SMODS.find_card('j_agar_lunala', false)[1] or { ability = { extra = {} } }).ability.extra.full_active
 end
 
 return {
