0d665b2cfc46214f4f5ddf4f28a3322d41e16224
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-17 07:00:42 +0200

    moved Yveltal to the correct config page

--- src/settings/contents.lua
+++ src/settings/contents.lua
@@ -27,12 +27,12 @@ return {
         { list = { "j_agar_dialga" }, label = function() return localize { type = "name_text", set = "Joker", key = "j_agar_dialga" } end, config_key = "dialga" },
         { list = { "j_agar_palkia" }, label = function() return localize { type = "name_text", set = "Joker", key = "j_agar_palkia" } end, config_key = "palkia" },
         { list = { "j_agar_xerneas" }, label = function() return localize { type = "name_text", set = "Joker", key = "j_agar_xerneas" } end, config_key = "xerneas" },
-        { list = { "j_agar_yveltal" }, label = function() return localize { type = "name_text", set = "Joker", key = "j_agar_yveltal" } end, config_key = "yveltal" },
       }
     },
     {
       title = function() return localize("agar_legendary_pokemon2") end,
       tiles = {
+        { list = { "j_agar_yveltal" }, label = function() return localize { type = "name_text", set = "Joker", key = "j_agar_yveltal" } end, config_key = "yveltal" },
         { list = { "j_agar_cosmog", "j_agar_cosmoem", "j_agar_solgaleo", "j_agar_lunala" }, label = function() return localize("agar_lunala_and_solgaleo") end, config_key = "cosmog" },
       }
     },
