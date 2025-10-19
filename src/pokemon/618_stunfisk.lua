30ffc1d8d87200171a4d526333b1aa1999dad214
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-19 11:07:57 +0200

    added stunfisk returning from the deck (with only a handful of bugs)

--- src/pokemon/618_stunfisk.lua
+++ src/pokemon/618_stunfisk.lua
@@ -13,23 +13,74 @@ local stunfisk = {
   gen = 5,
   calculate = function(self, card, context)
     if context.setting_blind then
-      -- -- We're a playing card now.
-      -- -- Okay so turns out becoming a playing card gets us rendered as a playing card,
-      -- -- and that crashes the game when it finds out we're not a real playing card.
-      -- -- We should probably become a real playing card because not being a playing card
-      -- -- crashes the game when it tries to find out if our hand is a Flush.
-      -- G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-      -- card.playing_card = G.playing_card
-      -- table.insert(G.playing_cards, card)
-
+      -- Okay so turns out becoming a playing card gets us rendered as a playing card,
+      -- and that crashes the game when it finds out we're not a real playing card.
+      -- We should probably become a real playing card because not being a playing card
+      -- crashes the game when it tries to find out if our hand is a Flush.
+      card.set_sprites = function() end
+      card:set_base(G.P_CARDS['D_2']) -- 2 of Diamonds won't be shown bc Stunfisk is a Hazard card anyways
+      -- We're a playing card now.
+      G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+      card.playing_card = G.playing_card
+      table.insert(G.playing_cards, card)
       -- We're in the deck now.
-      card.area = G.deck
+      card:set_card_area(G.deck)
       card.area:emplace(card)
+      -- Removing cards messes with the Joker calculating, which works off indexes, so we delay it
+      G.E_MANAGER:add_event(Event({
+        func = function()
+          G.jokers:remove_card(card)
+          return true
+        end
+      }))
     end
   end,
+  remove_from_deck = function(self, card, from_debuff)
+  end
 }
+
+local init = function()
+  local calculate_ref = SMODS.current_mod.calculate
+  SMODS.current_mod.calculate = function(self, context)
+    if calculate_ref then
+      calculate_ref(self, context)
+    end
+    if context.end_of_round and not context.individual and not context.repetition then
+      -- TODO: Loop through G.hand and G.discard as well
+      for _, card in ipairs(G.deck.cards) do
+        if card.config.center_key == 'j_agar_stunfisk' then
+          card.config.card = {}
+          card.config.card_key = nil
+          -- card.base = {}
+
+          G.playing_card = math.max((G.playing_card and G.playing_card - 1) or 0, 0)
+          -- card.playing_card = nil
+          -- TODO: Look into Card:remove() [line:4752] on how to make this save.
+          -- Currently breaks if you reload a save immediately after entering the shop.
+          for i, playing_card in ipairs(G.playing_cards) do
+            if playing_card == card then
+              table.remove(G.playing_cards, i)
+              break
+            end
+          end
+
+          card:set_card_area(G.jokers)
+          card.area:emplace(card)
+          -- Once again removing cards mid loop causes problems, so we delay it
+          G.E_MANAGER:add_event(Event({
+            func = function()
+              G.deck:remove_card(card)
+              return true
+            end
+          }))
+        end
+      end
+    end
+  end
+end
 
 return {
   enabled = agarmons_config.stunfisk or false,
+  init = init,
   list = { stunfisk }
 }
