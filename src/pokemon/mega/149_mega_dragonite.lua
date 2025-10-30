eaed0cae94bfd69ca11a2d6f7a5bfee6db20a722
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-30 17:31:05 +0100

    clean up mega dragonite

--- src/pokemon/mega/149_mega_dragonite.lua
+++ src/pokemon/mega/149_mega_dragonite.lua
@@ -1,4 +1,4 @@
--- 149-1 Mega Dragonite
+-- Mega Dragonite 149-1
 local mega_dragonite = {
   name = "mega_dragonite",
   pos = { x = 0, y = 1 },
@@ -10,7 +10,7 @@ local mega_dragonite = {
     if G.jokers and G.jokers.cards then
       other_joker_count = #G.jokers.cards - 1
     end
-    return { vars = { center.ability.extra.retriggers, other_joker_count * 2 } }
+    return { vars = { center.ability.extra.retriggers, other_joker_count * center.ability.extra.retriggers } }
   end,
   rarity = "poke_mega",
   cost = 12,
@@ -19,14 +19,13 @@ local mega_dragonite = {
   gen = 1,
   atlas = "AgarmonsJokers",
   blueprint_compat = true,
-  perishable_compat = true,
   calculate = function(self, card, context)
     if context.repetition and context.cardarea == G.play
         and #G.play.cards == 1 then
       local other_joker_count = #G.jokers.cards - 1
       if other_joker_count > 0 then
         return {
-          repetitions = other_joker_count * 2
+          repetitions = other_joker_count * card.ability.extra.retriggers
         }
       end
     end
