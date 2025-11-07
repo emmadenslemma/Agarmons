5436953cbb92cd4295fb293de58c8e13f1f4dbfd
Emma Vedel <emmavedelpedersen@gmail.com>
2025-11-07 19:41:46 +0100

    add jokerdisplay support to all regular pokemon (except baxcalibur) and the z-a mega jokers

--- src/jokerdisplay.lua
+++ src/jokerdisplay.lua
@@ -3,12 +3,14 @@ local jd_def = JokerDisplay.Definitions
 local subdir = "src/jokerdisplay/"
 local files = NFS.getDirectoryItems(SMODS.current_mod.path .. subdir)
 
+local joker_prefix = 'j_agar_'
+
 for _, file in ipairs(files) do
   local defs = assert(SMODS.load_file(subdir .. file))()
 
   if defs.dict then
     for key, item in pairs(defs.dict) do
-      jd_def[key] = item
+      jd_def[joker_prefix .. key] = item
     end
   end
 end
