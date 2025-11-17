92fbdf43805ba0709c11a9536d19b06bd417f206
Emma Vedel <emmavedelpedersen@gmail.com>
2025-11-17 14:04:21 +0100

    add centiskorch

---
+++ src/pokemon/gmax/851_centiskorch.lua
@@ -0,0 +1,51 @@
+-- G-Max Centiskorch 851
+local gmax_centiskorch = {
+  name = "gmax_centiskorch",
+  inject_prefix = "Gem",
+  config = { extra = { mult = 0, mult_mod = 4 } },
+  loc_txt = {
+    name = "{C:agar_gmax}G-MAX{} Centiskorch",
+    text = {
+      "If played hand has {C:attention}5{} scoring",
+      "cards, played cards permanently",
+      "gain {C:mult}+#3#{} Mult when scored",
+    }
+  },
+  loc_vars = function(self, info_queue, center)
+    type_tooltip(self, info_queue, center)
+    return { vars = { center.ability.extra.mult_mod } }
+  end,
+  rarity = "agar_gmax",
+  cost = 10,
+  stage = "Gigantamax",
+  ptype = "Fire",
+  gen = 8,
+  blueprint_compat = true,
+  poke_custom_values_to_keep = { "mult" },
+  calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.play
+        and #context.scoring_hand == 5 then
+      context.other_card.ability.perma_mult = (context.other_card.ability.perma_mult or 0) + card.ability.extra.mult_mod
+      return {
+        message = localize('k_upgrade_ex'),
+        colour = G.C.MULT
+      }
+    end
+  end,
+}
+
+local init = function()
+  AG.append_to_family("centiskorch", "gmax_centiskorch", true)
+  AG.gmax.evos["j_Gem_centiskorch"] = "j_Gem_gmax_centiskorch"
+
+  SMODS.Joker:take_ownership("j_Gem_centiskorch", {
+    gmax = "gmax_centiskorch",
+    poke_custom_values_to_keep = { "mult" },
+  }, true)
+end
+
+return {
+  enabled = (SMODS.Mods["GemPokermon"] or {}).can_load and Gem_config.Sizzlipede and agarmons_config.gmax or false,
+  init = init,
+  list = { gmax_centiskorch }
+}
