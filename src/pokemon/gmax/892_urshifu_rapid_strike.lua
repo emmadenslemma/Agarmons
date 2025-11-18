61a60dbbac8d6d103cf0ad787b448efc6fa7938c
Emma Vedel <emmavedelpedersen@gmail.com>
2025-11-18 05:08:56 +0100

    clean urshifu files

---
+++ src/pokemon/gmax/892_urshifu_rapid_strike.lua
@@ -0,0 +1,48 @@
+-- G-Max Urshifu Rapid Strike Style 892
+local gmax_urshifu_rapid_strike = {
+  name = "gmax_urshifu_rapid_strike",
+  inject_prefix = "Gem",
+  config = { extra = { Xmult_mod = 0.1 } },
+  loc_txt = {
+    name = "{C:agar_gmax}G-MAX{} Urshifu Rapid Strike",
+    text = {
+      "Scored cards give {X:mult,C:white} X#3# {} Mult",
+      "for every {C:chips}5 chips{} they have",
+    }
+  },
+  loc_vars = function(self, info_queue, center)
+    type_tooltip(self, info_queue, center)
+    return { vars = { center.ability.extra.Xmult_mod } }
+  end,
+  rarity = "agar_gmax",
+  cost = 30,
+  stage = "Gigantamax",
+  ptype = "Water",
+  gen = 8,
+  designer = "Gem",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.play and not context.end_of_round then
+      local total_chips = poke_total_chips(context.other_card)
+      local Xmult = (total_chips) * (card.ability.extra.Xmult_mod) / 5 + 1
+      if Xmult > 0 then
+        return {
+          x_mult = Xmult,
+          card = card
+        }
+      end
+    end
+  end,
+}
+
+local init = function()
+  AG.append_to_family("urshifu_rapid_strike", "gmax_urshifu_rapid_strike")
+
+  SMODS.Joker:take_ownership("j_Gem_urshifu_rapid_strike", { gmax = "gmax_urshifu_rapid_strike" }, true)
+end
+
+return {
+  enabled = (SMODS.Mods["GemPokermon"] or {}).can_load and Gem_config.Kubfu and agarmons_config.gmax or false,
+  init = init,
+  list = { gmax_urshifu_rapid_strike }
+}
---
+++ src/pokemon/gmax/892_urshifu_single_strike.lua
@@ -0,0 +1,61 @@
+-- G-Max Urshifu Single Strike Style 892
+local gmax_urshifu_single_strike = {
+  name = "gmax_urshifu_single_strike",
+  inject_prefix = "Gem",
+  config = { extra = { Xmult = 7, Xmult_mod = 1 } },
+  loc_txt = {
+    name = "{C:agar_gmax}G-MAX{} Urshifu Single Strike",
+    text = {
+      "{X:mult,C:white} X#3# {} Mult",
+      "Gains {X:mult,C:white} X#4# {} Mult when a discard is used.",
+      "Loses a third when a hand is played",
+    }
+  },
+  loc_vars = function(self, info_queue, center)
+    type_tooltip(self, info_queue, center)
+    return { vars = { center.ability.extra.Xmult, center.ability.extra.Xmult_mod } }
+  end,
+  rarity = "agar_gmax",
+  cost = 30,
+  stage = "Gigantamax",
+  ptype = "Dark",
+  gen = 8,
+  designer = "Gem",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+    -- Increase xmult when a discard is used
+    if context.pre_discard and not context.blueprint then
+      card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_mod
+      return {
+        message = localize('k_upgrade_ex'),
+        colour = G.C.RED,
+        delay = 0.45,
+        card = card
+      }
+    end
+
+    -- Give Xmult
+    if context.cardarea == G.jokers and context.joker_main then
+      return {
+        Xmult = card.ability.extra.Xmult
+      }
+    end
+
+    -- Lose a third of your xmult when hand is played
+    if context.after and not context.blueprint then
+      card.ability.extra.Xmult = card.ability.extra.Xmult * (2 / 3)
+    end
+  end,
+}
+
+local init = function()
+  AG.append_to_family("urshifu_single_strike", "gmax_urshifu_single_strike")
+
+  SMODS.Joker:take_ownership("Gem_urshifu_single_strike", { gmax = "gmax_urshifu_single_strike" }, true)
+end
+
+return {
+  enabled = (SMODS.Mods["GemPokermon"] or {}).can_load and Gem_config.Kubfu and agarmons_config.gmax or false,
+  init = init,
+  list = { gmax_urshifu_single_strike }
+}
