741117e78e1bedf02943606fc0f31b1d97f01094
Emma Vedel <emmavedelpedersen@gmail.com>
2025-11-19 14:37:53 +0100

    rename `booster_limit_mod` to `booster_limit`

--- src/backs/gmaxdeck.lua
+++ src/backs/gmaxdeck.lua
@@ -3,14 +3,14 @@ local gmaxdeck = {
   key = "gmaxdeck",
   atlas = "AgarmonsBacks",
   pos = { x = 0, y = 0 },
-  config = { consumables = { "c_agar_dynamaxband" }, booster_limit_mod = -1 },
+  config = { consumables = { "c_agar_dynamaxband" }, booster_limit = -1 },
   loc_vars = function(self)
-    return { vars = { localize { type = "name_text", set = "Spectral", key = "c_agar_dynamaxband" }, self.config.booster_limit_mod } }
+    return { vars = { localize { type = "name_text", set = "Spectral", key = "c_agar_dynamaxband" }, self.config.booster_limit } }
   end,
   apply = function(self)
     G.E_MANAGER:add_event(Event({
       func = function()
-        SMODS.change_booster_limit(self.config.booster_limit_mod)
+        SMODS.change_booster_limit(self.config.booster_limit)
         return true
       end
     }))
@@ -22,13 +22,13 @@ local gmaxsleeve = {
   key = "gmaxsleeve",
   atlas = "AgarmonsSleeves",
   pos = { x = 0, y = 0 },
-  config = { booster_limit_mod = -1 },
+  config = { booster_limit = -1 },
   loc_vars = function(self)
     local key = self.key
     if self.get_current_deck_key() == "b_agar_gmaxdeck" then
       key = self.key .. "_alt"
     end
-    return { key = key, vars = { localize { type = "name_text", set = "Spectral", key = "c_agar_dynamaxband" }, self.config.booster_limit_mod } }
+    return { key = key, vars = { localize { type = "name_text", set = "Spectral", key = "c_agar_dynamaxband" }, self.config.booster_limit } }
   end,
   apply = function(self)
     CardSleeves.Sleeve.apply(self)
@@ -38,7 +38,7 @@ local gmaxsleeve = {
           SMODS.find_card("c_agar_dynamaxband")[1]:set_edition({ negative = true }, true)
         else
           SMODS.add_card { key = "c_agar_dynamaxband" }
-          SMODS.change_booster_limit(self.config.booster_limit_mod)
+          SMODS.change_booster_limit(self.config.booster_limit)
         end
         return true
       end
