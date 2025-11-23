f76ff0fce26294a06b92f7674b7977349e6b2762
Emma Vedel <emmavedelpedersen@gmail.com>
2025-11-23 13:30:08 +0100

    disable terrakion

--- src/pokemon/639_terrakion.lua
+++ src/pokemon/639_terrakion.lua
@@ -22,7 +22,7 @@ local terrakion = {
     if context.end_of_round and context.cardarea == G.jokers and not context.blueprint then
       local cards = {}
       local deck = AG.list_utils.copy(G.deck.cards)
-      table.sort(deck, function (a, b) return a:get_nominal() < b:get_nominal() end)
+      table.sort(deck, function(a, b) return a:get_nominal() < b:get_nominal() end)
       local stones = 0
       for i = 1, math.min(card.ability.extra.destroy_mod, #deck) do
         cards[i] = deck[i]
@@ -49,6 +49,6 @@ local terrakion = {
 }
 
 return {
-  enabled = agarmons_config.terrakion or false,
+  enabled = false, -- agarmons_config.terrakion or false,
   list = { terrakion }
 }
