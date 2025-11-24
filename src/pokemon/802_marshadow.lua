2a881814b032f2340d4ce70d2b5198c02a9f32db
Emma Vedel <emmavedelpedersen@gmail.com>
2025-11-24 18:29:19 +0100

    fight the formatter

--- src/pokemon/802_marshadow.lua
+++ src/pokemon/802_marshadow.lua
@@ -13,20 +13,13 @@ local marshadow = {
       local compatible = other_joker and other_joker ~= center and other_joker.config.center.blueprint_compat
       local colour = compatible and mix_colours(G.C.GREEN, G.C.JOKER_GREY, 0.8) or mix_colours(G.C.RED, G.C.JOKER_GREY, 0.8)
       local text = ' ' .. localize('k_' .. (compatible and 'compatible' or 'incompatible')) .. ' '
+      ---@format disable-next
       main_end = {
-        {
-          n = G.UIT.C,
-          config = { align = "bm", minh = 0.4 },
-          nodes = {
-            {
-              n = G.UIT.C,
-              config = { ref_table = center, align = "m", colour = colour, r = 0.05, padding = 0.06 },
-              nodes = {
-                { n = G.UIT.T, config = { text = text, colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
-              }
-            }
-          }
-        }
+        { n = G.UIT.C, config = { align = "bm", minh = 0.4 }, nodes = {
+          { n = G.UIT.C, config = { ref_table = center, align = "m", colour = colour, r = 0.05, padding = 0.06 }, nodes = {
+            { n = G.UIT.T, config = { text = text, colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
+          }}
+        }}
       }
     end
     return { vars = { (center.ability.extra.effect_multiplier - 1) * 100 }, main_end = main_end }
