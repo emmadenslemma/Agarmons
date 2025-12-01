17a0f006ec7e488f5e7a3ed2256bd7e4f98085ea
Emma Vedel <emmavedelpedersen@gmail.com>
2025-12-01 18:28:24 +0100

    fix function lib load orders

--- src/functions/b_list_utils.lua
+++ src/functions/b_list_utils.lua
@@ -7,7 +7,7 @@ end
 function AG.list_utils.map(list, func)
   new_list = {}
   for _, v in pairs(list) do
-    new_list[#new_list + 1] = func(v)
+    new_list[#new_list+1] = func(v)
   end
   return new_list
 end
@@ -18,6 +18,7 @@ function AG.list_utils.for_each(list, func)
   end
 end
 
+---@deprecated
 function AG.list_utils.copy(list)
   return AG.list_utils.map(list, AG.list_utils.id)
 end
@@ -26,7 +27,7 @@ function AG.list_utils.filter(list, func)
   new_list = {}
   for _, v in pairs(list) do
     if func(v) then
-      new_list[#new_list + 1] = v
+      new_list[#new_list+1] = v
     end
   end
   return new_list
