2f81d81449ff826f11bdbe074ec213f087abede2
Emma Vedel <emmavedelpedersen@gmail.com>
2025-12-05 23:36:47 +0100

    add drampa and mega drampa

--- src/functions/effects.lua
+++ src/functions/effects.lua
@@ -6,3 +6,32 @@ function AG.effects.apply_scry_hand_effects()
       or SMODS.find_card('j_agar_azelf', false)[1]
       or (SMODS.find_card('j_agar_lunala', false)[1] or { ability = { extra = {} } }).ability.extra.full_active
 end
+
+function AG.effects.apply_force_jumbo_packs()
+  return next(SMODS.find_card('j_agar_drampa'))
+end
+
+function AG.effects.apply_force_mega_packs()
+  return next(SMODS.find_card('j_agar_mega_drampa'))
+end
+
+local function try_upgrade_pack(center, from, to)
+  local upgraded_key = center.key:gsub(from, to)
+  return G.P_CENTERS[upgraded_key]
+      or G.P_CENTERS[upgraded_key:gsub('_%d$', '_1')] -- default to the first variant
+end
+
+local get_pack_ref = get_pack
+function get_pack(_key, _type)
+  local center = get_pack_ref(_key, _type)
+  if _key == 'shop_pack' then
+    if AG.effects.apply_force_jumbo_packs() then
+      center = try_upgrade_pack(center, "normal", "jumbo") or center
+    end
+    if AG.effects.apply_force_mega_packs() then
+      center = try_upgrade_pack(center, "normal", "mega") or center
+      center = try_upgrade_pack(center, "jumbo", "mega") or center
+    end
+  end
+  return center
+end
---
+++ src/pokemon/780_drampa.lua
@@ -0,0 +1,55 @@
+local drampa = {
+  name = "drampa",
+  config = { extra = { boosters = 1 } },
+  loc_vars = function(self, info_queue, card)
+    type_tooltip(self, info_queue, card)
+    return { vars = { card.ability.extra.boosters } }
+  end,
+  rarity = 3,
+  cost = 8,
+  stage = "Basic",
+  ptype = "Dragon",
+  gen = 7,
+  add_to_deck = function(self, card, from_debuff)
+    SMODS.change_booster_limit(card.ability.extra.boosters)
+  end,
+  remove_from_deck = function(self, card, from_debuff)
+    SMODS.change_booster_limit(-card.ability.extra.boosters)
+  end,
+  designer = "CBMX",
+  megas = { "mega_drampa" },
+}
+
+local mega_drampa = {
+  name = "mega_drampa",
+  pos = { x = 8, y = 2 },
+  soul_pos = { x = 9, y = 2 },
+  config = { extra = { boosters = 1, booster_choice_mod = 1 } },
+  loc_vars = function(self, info_queue, card)
+    type_tooltip(self, info_queue, card)
+    return { vars = { card.ability.extra.boosters, card.ability.extra.booster_choice_mod } }
+  end,
+  rarity = "poke_mega",
+  cost = 12,
+  stage = "Mega",
+  ptype = "Dragon",
+  gen = 7,
+  atlas = "AgarmonsJokers",
+  add_to_deck = function(self, card, from_debuff)
+    SMODS.change_booster_limit(card.ability.extra.boosters)
+    G.GAME.modifiers.booster_choice_mod =
+        (G.GAME.modifiers.booster_choice_mod or 0) + card.ability.extra.booster_choice_mod
+  end,
+  remove_from_deck = function(self, card, from_debuff)
+    SMODS.change_booster_limit(-card.ability.extra.boosters)
+    G.GAME.modifiers.booster_choice_mod =
+        math.max(0, (G.GAME.modifiers.booster_choice_mod or 0) - card.ability.extra.booster_choice_mod)
+  end,
+  artist = "KingOfThe-X-Roads",
+  designer = "Gem",
+}
+
+return {
+  config_key = "drampa",
+  list = { drampa, mega_drampa }
+}
