dde726b50ec0db2a2013443c1c312d27830b6019
Emma Vedel <emmavedelpedersen@gmail.com>
2025-12-17 21:21:34 +0100

    learn about the power of atan

--- assets/shaders/fuse.fs
+++ assets/shaders/fuse.fs
@@ -18,10 +18,12 @@ vec4 effect(vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords)
 {
     MY_HIGHP_OR_MEDIUMP vec4 tex = Texel(texture, texture_coords);
     MY_HIGHP_OR_MEDIUMP vec2 uv = (((texture_coords)*(image_details)) - texture_details.xy*texture_details.ba)/texture_details.ba;
-    
-    vec2 offset_uv = uv / offset;
 
-    float y = 1.0 - offset_uv.x;
+    float angle = atan(offset.x/offset.y);
+    
+    // vec2 offset_uv = uv - vec2(0.5, 0.5);
+
+    float y = 1.0 - uv.x * angle;
     tex.a = tex.a * plot(uv, y);
     tex.rgb = vec3(0.6, 0.7, 1.0);
 
