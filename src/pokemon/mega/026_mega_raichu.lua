6a9106252e7b152e294a352e883be6107aaa195d
Emma Vedel <emmavedelpedersen@gmail.com>
2025-12-29 18:06:51 +0100

    add mega raichu x

--- src/pokemon/mega/026_mega_raichu.lua
+++ src/pokemon/mega/026_mega_raichu.lua
@@ -1,19 +1,31 @@
+local function get_current_interest()
+  local interest_cap = AG.effects.uncap_interest() and 0 / 1 or G.GAME.interest_cap / 5
+  return math.min(math.floor(G.GAME.dollars / 5), interest_cap)
+end
+
 -- Mega Raichu X 026-2
 local mega_raichu_x = {
   name = "mega_raichu_x",
   agar_inject_prefix = "poke",
   pos = { x = 2, y = 9 },
   soul_pos = { x = 3, y = 9 },
-  config = { extra = {} },
+  config = { extra = { Xmult_mod = 0.5, per_interest = 1 } },
   loc_txt = {
     name = "Mega Raichu X",
     text = {
-      "{C:dark_edition}???",
+      "{C:white,X:mult}X#1#{} Mult for every",
+      "{C:money}$#2#{} of interest you have",
+      "{C:inactive}(Currently {C:white,X:mult}X#3#{C:inactive} Mult)",
     }
   },
+  get_x_mult = function(self, card)
+    local interest_mod = get_current_interest() / card.ability.extra.per_interest
+    local Xmult_mod = card.ability.extra.Xmult_mod
+    return 1 + Xmult_mod * interest_mod
+  end,
   loc_vars = function(self, info_queue, card)
     type_tooltip(self, info_queue, card)
-    return { vars = {} }
+    return { vars = { card.ability.extra.Xmult_mod, card.ability.extra.per_interest, self:get_x_mult(card) } }
   end,
   rarity = "poke_mega",
   cost = 12,
@@ -23,6 +35,11 @@ local mega_raichu_x = {
   atlas = "AtlasJokersSeriesAGen01",
   blueprint_compat = true,
   calculate = function(self, card, context)
+    if context.joker_main then
+      return {
+        Xmult = self:get_x_mult(card)
+      }
+    end
   end,
   artist = "MyDude_YT",
 }
@@ -52,14 +69,14 @@ local mega_raichu_y = {
 }
 
 local function init()
-  -- AG.append_to_family("raichu", "mega_raichu_x", true)
+  AG.append_to_family("raichu", "mega_raichu_x", true)
   AG.append_to_family("raichu", "mega_raichu_y", true)
-  SMODS.Joker:take_ownership("poke_raichu", { megas = { --[["mega_raichu_x",]] "mega_raichu_y" } }, true)
+  SMODS.Joker:take_ownership("poke_raichu", { megas = { "mega_raichu_x", "mega_raichu_y" } }, true)
 end
 
 return {
   can_load = agarmons_config.new_megas,
   init = init,
-  list = { --[[mega_raichu_x,]] mega_raichu_y },
+  list = { mega_raichu_x, mega_raichu_y },
   family = {},
 }
