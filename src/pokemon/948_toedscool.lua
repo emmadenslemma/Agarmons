25f95b06e2512632eb52833c160dde89ee4936b5
Emma Vedel <emmavedelpedersen@gmail.com>
2026-01-12 20:44:15 +0100

    make toedscruel work with the big fish

--- src/pokemon/948_toedscool.lua
+++ src/pokemon/948_toedscool.lua
@@ -45,7 +45,72 @@ local toedscruel = {
   end
 }
 
+local init = function()
+  -- This is to account for using Xmult_mod and returning a localized a_x_mult message manually (i.e. Gyarados)
+  -- or using Xmult_mod to return a message instead of 2 separate effect messages (i.e. Kingdra)
+  local move_xmult_effects = function(from, to)
+    to['x_mult'] = from['x_mult']
+    to['Xmult'] = from['Xmult']
+    to['xmult'] = from['xmult']
+    to['x_mult_mod'] = from['x_mult_mod']
+    to['Xmult_mod'] = from['Xmult_mod']
+
+    from['x_mult'] = nil
+    from['Xmult'] = nil
+    from['xmult'] = nil
+    from['x_mult_mod'] = nil
+    from['Xmult_mod'] = nil
+
+    -- We assume that if you have a message it's for the Xmult part
+    to['message'] = from['message']
+    to['colour'] = from['colour']
+
+    from['message'] = nil
+    from['colour'] = nil
+
+    -- If you've previously had a message for 2 effects, make them appear without a standalone message
+    if to['message'] and to['Xmult_mod'] and (from['mult_mod'] or from['chip_mod']) then
+      if not to['Xmult'] then
+        -- We don't need a specific message for one Xmult value
+        to['message'] = nil
+        to['colour'] = nil
+        to['Xmult'] = to['Xmult_mod']
+        to['Xmult_mod'] = nil
+      end
+
+      if not from['mult'] then
+        from['mult'] = from['mult_mod']
+        from['mult_mod'] = nil
+      end
+      if not from['chips'] then
+        from['chips'] = from['chip_mod']
+        from['chip_mod'] = nil
+      end
+    end
+  end
+
+  local effect_has_xmult = function(effect)
+    return (effect['x_mult']
+      or effect['Xmult']
+      or effect['xmult']
+      or effect['x_mult_mod']
+      or effect['Xmult_mod'])
+  end
+
+  AG.hookaroundfunc(SMODS, 'calculate_effect', function(orig, effect, ...)
+    if AG.mycelium_might and effect_has_xmult(effect) then
+      local stored_effect = {}
+
+      move_xmult_effects(effect, stored_effect)
+
+      table.insert(AG.stored_xmult_effects, { stored_effect, ... })
+    end
+    return orig(effect, ...)
+  end)
+end
+
 return {
   config_key = "toedscool",
+  init = init,
   list = { toedscool, toedscruel }
 }
