c605cedda5a6ea4c5df93a0c7f2035e5ab3cd93c
Emma Vedel <emmavedelpedersen@gmail.com>
2026-01-30 20:11:32 +0100

    calm the intellisense

--- src/functions/effects.lua
+++ src/functions/effects.lua
@@ -1,10 +1,10 @@
 AG.effects = {}
 
 function AG.effects.apply_scry_hand_effects()
-  return SMODS.find_card('j_agar_uxie', false)[1]
-      or SMODS.find_card('j_agar_mesprit', false)[1]
-      or SMODS.find_card('j_agar_azelf', false)[1]
-      or (SMODS.find_card('j_agar_lunala', false)[1] or { ability = { extra = {} } }).ability.extra.full_active
+  return SMODS.find_card('j_agar_uxie')[1]
+      or SMODS.find_card('j_agar_mesprit')[1]
+      or SMODS.find_card('j_agar_azelf')[1]
+      or (SMODS.find_card('j_agar_lunala')[1] or { ability = { extra = {} } }).ability.extra.full_active
 end
 
 function AG.effects.apply_force_jumbo_packs()
