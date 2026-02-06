3359617946e5371dc422b9854c6453b325ffbde6
Emma Vedel <emmavedelpedersen@gmail.com>
2026-02-06 11:02:51 +0100

    added self sufficiency to magerna

--- src/pokemon/801_magearna.lua
+++ src/pokemon/801_magearna.lua
@@ -5,6 +5,9 @@ local magearna = {
   loc_txt = {
     name = "Magearna",
     text = {
+      "When {C:attention}Blind{} is selected,",
+      "destroys Joker to the right",
+      "{br:2}ERROR - CONTACT STEAK",
       "Gains {C:white,X:mult}X#1#{} Mult when",
       "a Joker is destroyed",
       "{C:inactive}(Currently {C:white,X:mult}X#2#{C:inactive} Mult)",
@@ -21,6 +24,13 @@ local magearna = {
   gen = 7,
   blueprint_compat = true,
   calculate = function(self, card, context)
+    if context.setting_blind and not context.blueprint then
+      local idx = get_index(G.jokers.cards, card)
+      local to_destroy = G.jokers.cards[idx + 1]
+      if to_destroy and not to_destroy.getting_sliced then
+        SMODS.destroy_cards(to_destroy)
+      end
+    end
     if context.joker_main then
       return {
         Xmult = card.ability.extra.Xmult
@@ -29,7 +39,8 @@ local magearna = {
     if context.joker_type_destroyed and not context.blueprint then
       card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_mod
       return {
-        message = localize('agar_soul_heart')
+        message = localize('agar_soul_heart'),
+        colour = G.ARGS.LOC_COLOURS.fairy
       }
     end
   end,
