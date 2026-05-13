9f337c5377d2411a8bdac8ec895746a25f57fb89
Emma Vedel <emmavedelpedersen@gmail.com>
2026-05-13 02:39:08 +0200

    convert blueorb and adamantcrystal to legendary orb templates

--- src/consumables/92_blueorb.lua
+++ src/consumables/92_blueorb.lua
@@ -1,11 +1,7 @@
-local blueorb = {
+local blueorb = AG.legendary_orb {
   name = "blueorb",
   key = "blueorb",
-  set = "Spectral",
   pos = { x = 3, y = 0 },
-  helditem = true,
-  saveable = true,
-  config = { extra = { usable = true, active = false } },
   loc_txt = {
     name = "Blue Orb",
     text = {
@@ -14,54 +10,10 @@ local blueorb = {
       "{C:inactive}(Usable once per round)",
     }
   },
-  loc_vars = function(self, info_queue, center)
-    if center.ability.extra.active then
-      return { key = "c_agar_blueorb_active" }
-    end
-  end,
   atlas = "AgarmonsConsumables",
-  cost = 4,
-  hidden = true,
-  soul_set = "Item",
-  soul_rate = .005,
-  use = function(self, card)
-    local target_key = card.ability.extra.active and "j_agar_primal_kyogre" or "j_agar_kyogre"
-    local evolve_to = card.ability.extra.active and "j_agar_kyogre" or "j_agar_primal_kyogre"
 
-    local target = poke_find_leftmost_or_highlighted(target_key)
-
-    poke_evolve(target, evolve_to)
-
-    card.ability.extra.active = not card.ability.extra.active
-    card.ability.extra.usable = false
-  end,
-  can_use = function(self, card)
-    if not card.ability.extra.usable then return false end
-
-    local target_key = card.ability.extra.active and "j_agar_primal_kyogre" or "j_agar_kyogre"
-
-    return poke_find_leftmost_or_highlighted(target_key)
-  end,
-  calculate = function(self, card, context)
-    if context.end_of_round and not card.ability.extra.usable then
-      card.ability.extra.usable = true
-      card_eval_status_text(card, "extra", nil, nil, nil, { message = localize("k_reset") })
-    end
-  end,
-  keep_on_use = function(self, card)
-    return true
-  end,
-  in_pool = function(self)
-    return next(SMODS.find_card("j_agar_kyogre"))
-  end,
-  remove_from_deck = function(self, card, from_debuff)
-    if card.ability.extra.active then
-      local target = SMODS.find_card("j_agar_primal_kyogre")[1]
-      if target then
-        poke_evolve(target, "j_agar_kyogre")
-      end
-    end
-  end
+  agar_base_key = "j_agar_kyogre",
+  agar_form_key = "j_agar_primal_kyogre",
 }
 
 return {
